54025bb2dc5ba4562cc90e1ba3fb79a7caf144ad
Keshava11 <mfsi.ravi@gmail.com>
2017-08-17 19:18:09 +0530

    Refactored code to demo few more Kotlin constructs. Updated kotlin runtime version

--- app/src/main/java/me/myatminsoe/bmicalculator/MainActivity.kt
+++ app/src/main/java/me/myatminsoe/bmicalculator/MainActivity.kt
@@ -9,81 +9,55 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.webkit.WebView
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar
-import java.math.RoundingMode
-import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
 
+    private val LICENSES_FILE_PATH: String = "file:///android_asset/licenses.html"
+
+    companion object {
+        private val CATEGORY_CODE_MAP: HashMap<Int, String> = HashMap(5)
+
+        init {
+            CATEGORY_CODE_MAP.put(R.color.blue, "Underweight")
+            CATEGORY_CODE_MAP.put(R.color.green, "Healthy")
+            CATEGORY_CODE_MAP.put(R.color.yellow, "Overweight")
+            CATEGORY_CODE_MAP.put(R.color.orange, "Obese")
+            CATEGORY_CODE_MAP.put(R.color.red, "Extremely Obese")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sbWeight.setOnProgressChangeListener(object : DiscreteSeekBar.OnProgressChangeListener {
-            override fun onProgressChanged(seekBar: DiscreteSeekBar, value: Int, fromUser: Boolean) {
-                tvWeight.text = value.toString() + " Kg"
-            }
-
-            override fun onStartTrackingTouch(seekBar: DiscreteSeekBar) {
-
-            }
-
-            override fun onStopTrackingTouch(seekBar: DiscreteSeekBar) {
-
-            }
-        })
-
-        sbHeight.setOnProgressChangeListener(object : DiscreteSeekBar.OnProgressChangeListener {
-            override fun onProgressChanged(seekBar: DiscreteSeekBar, value: Int, fromUser: Boolean) {
-                tvHeight.text = value.toString() + " cm"
-            }
-
-            override fun onStartTrackingTouch(seekBar: DiscreteSeekBar) {
-
-            }
-
-            override fun onStopTrackingTouch(seekBar: DiscreteSeekBar) {
-
-            }
-        })
+        sbWeight.setOnProgressChangeListener(BMIProgressChangeListener(tvWeight, getString(R.string.unit_kg)))
+        sbHeight.setOnProgressChangeListener(BMIProgressChangeListener(tvHeight, getString(R.string.unit_cm)))
     }
 
     fun calculate(v: View) {
         val weight = sbWeight.progress.toDouble()
         val height = sbHeight.progress.toDouble() / 100
-        val df = DecimalFormat("#.#")
-        df.roundingMode = RoundingMode.HALF_UP
-        val bmi = weight / (height * height)
-        tvBMI.text = "BMI\n" + df.format(bmi)
-        when (bmi.toInt()) {
-            in 0..18-> {
-                tvResult.text = "Underweight"
-                tvResult.setTextColor(ContextCompat.getColor(this, R.color.blue))
-                (tvBMI.background as GradientDrawable).setColor(ContextCompat.getColor(this, R.color.blue))
-            }
-            in 19..24-> {
-                tvResult.text = "Healthy"
-                tvResult.setTextColor(ContextCompat.getColor(this, R.color.green))
-                (tvBMI.background as GradientDrawable).setColor(ContextCompat.getColor(this, R.color.green))
-            }
-            in 25..29 -> {
-                tvResult.text = "Overweight"
-                tvResult.setTextColor(ContextCompat.getColor(this, R.color.yellow))
-                (tvBMI.background as GradientDrawable).setColor(ContextCompat.getColor(this, R.color.yellow))
-            }
-            in 30..39 -> {
-                tvResult.text = "Obese"
-                tvResult.setTextColor(ContextCompat.getColor(this, R.color.orange))
-                (tvBMI.background as GradientDrawable).setColor(ContextCompat.getColor(this, R.color.orange))
-            }
-            else -> {
-                tvResult.text = "Extremely Obese"
-                tvResult.setTextColor(ContextCompat.getColor(this, R.color.red))
-                (tvBMI.background as GradientDrawable).setColor(ContextCompat.getColor(this, R.color.red))
-            }
-        }
+
+        // Calculate bmi
+        val bmiDetails = BMIUtils.calculateBMI(weight, height)
+
+        // Update the TextView content
+        tvBMI.text = bmiDetails.first
+
+        // Fetch the category identifier color code
+        val categoryIdentifierColorCode: Int = BMIUtils.getCategoryIdentifier(bmiDetails.second)
+
+        // Update Category TextView's text and background
+        updateViews(CATEGORY_CODE_MAP.get(categoryIdentifierColorCode), categoryIdentifierColorCode)
+    }
+
+    private fun updateViews(category: String?, colorCode: Int) {
+        tvResult.text = category; //
+        tvResult.setTextColor(ContextCompat.getColor(this, colorCode))
+        (tvBMI.background as GradientDrawable).setColor(ContextCompat.getColor(this, colorCode))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -94,13 +68,32 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         if (item?.itemId == R.id.action_license) {
             val wv = WebView(this)
-            wv.loadUrl("file:///android_asset/licenses.html")
+            wv.loadUrl(LICENSES_FILE_PATH)
             val builder = AlertDialog.Builder(this)
-                    .setTitle("Licenses")
+                    .setTitle(getString(R.string.licences))
                     .setView(wv)
-                    .setPositiveButton("OK") { dialog, which -> dialog.dismiss() }
+                    .setPositiveButton(getString(R.string.ok)) { dialog, which -> dialog.dismiss() }
             builder.show()
         }
         return super.onOptionsItemSelected(item)
     }
+
+    /**
+     * Inner class for monitoring changes in seek bar progress
+     */
+    inner class BMIProgressChangeListener(var tv: TextView, var unit: String) : DiscreteSeekBar.OnProgressChangeListener {
+
+        override fun onProgressChanged(seekBar: DiscreteSeekBar?, value: Int, fromUser: Boolean) {
+            tv.text = this@MainActivity.getString(R.string.bmi_value_formatted, value, unit)
+        }
+
+        override fun onStartTrackingTouch(seekBar: DiscreteSeekBar?) {
+
+        }
+
+        override fun onStopTrackingTouch(seekBar: DiscreteSeekBar?) {
+
+        }
+    }
+
 }
